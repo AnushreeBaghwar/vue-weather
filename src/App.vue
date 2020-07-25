654a029aa3d16b54937d9eb7d80e96284cceb3cd
Anushree Baghwar <anushreebaghwar03@gmail.com>
2020-07-25 15:36:08 +0530

    creating weather app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     New York,USA <br>
+    wednesday 1 april
+    <h1>26°</h1>
+    Rain
+    <input type="search" name="" id="search-bar" placeholder="Search...">
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      api_key:'becd0525a5b19e7ed8384f10c3169c9c'
+    }
   }
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+body{
+background-image:url('./assets/weather2.jpg');
+background-size: cover;
+background-position: bottom;
+
+}
+input#search-bar {
+    
+    padding:10px;
+    border-radius:20px;
+    border-style:none;
+    width:50%;
+    outline:none;
+    box-shadow:1px 2px 2px #3e3636;
+    position:relative;
+    top:50px;
+    left:25%;
+}
+
 </style>

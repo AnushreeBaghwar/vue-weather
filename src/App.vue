6fa2cee635dee004c1695ff89578a816e8bd217f
Anushree Baghwar <anushreebaghwar03@gmail.com>
2020-07-29 16:26:16 +0530

    added css and icon

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
    
    <div class="place_name">{{weather.name}}</div>
    <div class="date">{{weather.date}}</div>
+   <div id="icon">
+   <!-- {{weather.icon}} -->
+   <img id="wIcon" v-bind:src="'http://openweathermap.org/img/w/'+weather.icon+'.png'" alt="Weather description" ></div>
    <div class="temperature">{{weather.temp}}°C</div>
    <div class="climate">{{weather.climate}}</div>
 
@@ -24,13 +27,15 @@ export default {
       api_key:'becd0525a5b19e7ed8384f10c3169c9c',
       zip:null,
       url : 'https://api.openweathermap.org/data/2.5/weather?',
+      
       // +'zip=736205'+',IN&appid=',
       weather:{
         name:null,
         date:null,
         temp:null,
         climate:null,
-        desc:null
+        desc:null,
+        icon:null
       }
    }
     
@@ -51,7 +56,10 @@ export default {
       //  this.weather.temp=res.data.main.temp;
        this.weather.climate=res.data.weather[0].main;
       var d=((res.data.dt)*1000);
-       this.weather.date=new Date(d);     
+       this.weather.date=new Date(d);
+       this.weather.icon=res.data.weather[0].icon;
+       
+       
       });
       
         
@@ -69,10 +77,23 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+#app {
+    background:rgba(0,0,0,0.2);
+    height:600px;
+    margin:30px;
+    border:1px solid #00b4ffd4;
+    box-shadow:1px 2px 2px #00f0ffd6;
+    
+}
+img#wIcon {
+    width:120px;
+    position:relative;
+    top:100px;
+    left:30%;
+}
 body{
   
 background-image:url('./assets/bg.jpg');
-
 background-repeat:repeat-y;
 background-size: cover;
 
@@ -105,13 +126,21 @@ input#search-bar {
     
 }
 .temperature {
+  background:rgba(0,0,0,0.2);
+  border: 1px solid #bebebb;
+      border: 1px solid #a77df99c;
+    height: 100px;
+    border-radius: 10px;
+    width: fit-content;
+    box-shadow: 1px 2px 5px #a978e0;
     position:relative;
     text-align:center;
-    top:150px;
+    top: 120px;
     font-weight:bold;
     font-family:cursive;
 font-size:50px;
       color: bisque;
+      left:30%;
 }
 .climate {
     text-align:center;
